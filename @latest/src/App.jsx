1c21e43f093974f5f6b633816cc36c18507d7135
Ravindu-Buddhika <05ravindubuddhika@gmail.com>
2026-04-16 23:56:53 +0530

    added new routings

--- @latest/src/App.jsx
+++ @latest/src/App.jsx
@@ -1,10 +1,43 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import VehiclePage from './pages/VehiclePage'; 
+import Login from './pages/Login'; 
+import Register from './pages/Register';
 import BookingModal from './components/BookingModal'; 
+import PaymentPage from './pages/PaymentPage';
+import MyBookings from './pages/MyBookings'; // අලුතින් සාදන පේජ් එක
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+const AppContent = ({ handleShowDetails, showModal, selectedCar, handleClose }) => {
+  const location = useLocation();
+  
+  const isAuthPage = location.pathname === '/login' || location.pathname === '/register';
+
+  return (
+    <>
+      {!isAuthPage && <Navbar role="customer" />} 
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cars" element={<VehiclePage onShowDetails={handleShowDetails} />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/payment" element={<PaymentPage />} /> 
+        <Route path="/my-bookings" element={<MyBookings />} /> 
+      </Routes>
+
+      {!isAuthPage && (
+        <BookingModal 
+          show={showModal} 
+          car={selectedCar} 
+          handleClose={handleClose} 
+        />
+      )}
+    </>
+  );
+};
 
 function App() {
   const [selectedCar, setSelectedCar] = useState(null);
@@ -22,20 +55,11 @@ function App() {
 
   return (
     <Router>
-      <Navbar role="customer" /> 
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route 
-          path="/cars" 
-          element={<VehiclePage onShowDetails={handleShowDetails} />} 
-        />
-      </Routes>
-
-      <BookingModal 
-        show={showModal} 
-        car={selectedCar} 
-        handleClose={handleClose} 
+      <AppContent 
+        handleShowDetails={handleShowDetails}
+        showModal={showModal}
+        selectedCar={selectedCar}
+        handleClose={handleClose}
       />
     </Router>
   );
